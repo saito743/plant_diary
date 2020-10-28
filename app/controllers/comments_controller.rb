ffa99241ed7f68a978f8b8f0981724a641500c73
saito743 <yuka.saito21@gmail.com>
2020-10-28 05:19:32 +0000

    comment削除機能追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,7 +12,10 @@ class CommentsController < ApplicationController
 	end
 
 	def destroy
+		Comment.find_by(id: params[:id],plant_id: params[:plant_id]).delete
+		redirect_to request.referer
 	end
+
 private
 	def comment_params
 		params.require(:comment).permit(:body,:comment_image, :user_id, :pkant_id)
