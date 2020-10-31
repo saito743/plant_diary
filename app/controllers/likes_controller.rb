d0c96552eb963fa7afb5292d1092ca5e7531d67a
saito743 <yuka.saito21@gmail.com>
2020-10-31 10:55:16 +0000

    いいね　非同期通信

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,17 +3,15 @@ class LikesController < ApplicationController
 before_action :authenticate_user!
 
 	def create
-		plant = Plant.find(params[:plant_id])
-		like = Like.new(plant_id: plant.id, user_id: current_user.id)
+		@plant = Plant.find(params[:plant_id])
+		like = Like.new(plant_id: @plant.id, user_id: current_user.id)
 		like.save
-		redirect_to request.referer
 	end
 
 	def destroy
-		plant = Plant.find(params[:plant_id])
-		like = Like.find_by(plant_id: plant.id, user_id: current_user.id)
+		@plant = Plant.find(params[:plant_id])
+		like = Like.find_by(plant_id: @plant.id, user_id: current_user.id)
 		like.destroy
-		redirect_to request.referer
 	end
 private
 	def like_params
