073cdf7e23637ac8a730e763a60e0dc82ec16af7
saito743 <yuka.saito21@gmail.com>
2020-11-13 16:16:22 +0000

    フォロー非同期　保留

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -3,18 +3,18 @@ class FollowsController < ApplicationController
 
 	def create
 		current_user.follow(params[:user_id])
-		@user = User.find(params[:user_id])	#詳細ページのフォロワー数カウントで使用
-		@user_follow = User.find_by(id: current_user.id)	#フォロー一覧ページで使用
-		@following_users = @user_follow.following_user
-		@followed_users = @user_follow.followed_user
+		redirect_to request.referer
+		#@user_follow = User.find_by(id: current_user.id)	#フォロー一覧ページがうまく動かないので保留
+		#@following_users = @user_follow.following_user
+		#@followed_users = @user_follow.followed_user
 	end
 
 	def destroy
 		current_user.unfollow(params[:user_id])
-		@user = User.find(params[:user_id])
-		@user_follow = User.find_by(id: current_user.id)
-		@following_users = @user_follow.following_user
-		@followed_users = @user_follow.followed_user
+		redirect_to request.referer
+		#@user_follow = User.find_by(id: current_user.id)
+		#@following_users = @user_follow.following_user
+		#@followed_users = @user_follow.followed_user
 	end
 
 	def follower
